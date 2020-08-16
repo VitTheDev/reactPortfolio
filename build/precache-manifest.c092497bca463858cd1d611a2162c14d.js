eff544596b59bc47559c8fe66c8ad410f7c93376
Vit <busek.vit@gmail.com>
2020-08-16 22:01:26 +0200

    create build folder

---
+++ build/precache-manifest.c092497bca463858cd1d611a2162c14d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9b3671cec4d2ebb5c66a559ca1aa95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f73da3f77f3de938ad58",
+    "url": "/static/css/main.284b31d5.chunk.css"
+  },
+  {
+    "revision": "a1c3c8b943d4b95c6658",
+    "url": "/static/js/2.ad6a2231.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ad6a2231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73da3f77f3de938ad58",
+    "url": "/static/js/main.3d52695d.chunk.js"
+  },
+  {
+    "revision": "0934e5a0629121a12638",
+    "url": "/static/js/runtime-main.c542aba9.js"
+  },
+  {
+    "revision": "2da0448af4837f8091c4e112055b13ea",
+    "url": "/static/media/me-min.2da0448a.jpg"
+  }
+]);
